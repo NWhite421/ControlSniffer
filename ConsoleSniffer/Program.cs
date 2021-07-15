74eada098603766891b7840f22bc14fdca63d1f4
nwhitemisc <nwhitemisc@gmail.com>
2021-07-15 14:17:37 -0400

    Working version

--- ConsoleSniffer/Program.cs
+++ ConsoleSniffer/Program.cs
@@ -1,12 +1,252 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace ConsoleSniffer
 {
     class Program
     {
+        #region private variables
+        /// <summary>
+        /// Logger
+        /// </summary>
+        private static readonly ILog log = log4net.LogManager.GetLogger("Program.cs");
+
+        /// <summary>
+        /// Starting job number
+        /// </summary>
+        private static string StartingJobNumber { get; set; }
+        
+        /// <summary>
+        /// Ending job number
+        /// </summary>
+        private static string EndingJobNumber { get; set; }
+
+        /// <summary>
+        /// City filter
+        /// </summary>
+        private static List<string> Cities = new List<string> { };
+
+        /// <summary>
+        /// The point output file;
+        /// </summary>
+        private static string OutputFilePath { get; set; }
+
+        /// <summary>
+        /// Point keyword whitelist
+        /// </summary>
+        private static readonly List<string> KeywordWhitelist = new List<string> { "PCP", ",CM", "NLD" };
+
+        /// <summary>
+        /// Point keywork blacklist
+        /// </summary>
+        private static readonly List<string> KeywordBlacklist = new List<string> { ",IRC","SET" };
+
+        private static List<string> FoundPoints = new List<string> { };
+        #endregion
+
         static void Main(string[] args)
         {
+            log.Debug("Program started");
             Console.WriteLine("Hello World!");
+
+            EstablishJobParams();
+
+            OutputFilePath = $"point output {DateTime.Now:yyyy-MM-dd-HH-mm-ss} {String.Join("-", Cities.ToArray())}.csv";
+
+            string recordPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Records";
+            foreach (string file in Directory.GetFiles(recordPath, "*.csv", SearchOption.TopDirectoryOnly))
+            {
+                GatherAndProcessProjects(file);
+            }
+
+            File.WriteAllLines(OutputFilePath, FoundPoints);
+            Console.WriteLine("Processing completed.\n" +
+                $"This program found {FoundPoints.Count} related points");
+
+            log.Debug("Program ended");
+            Console.Write("Program finished, press any key to exit");
+            Console.ReadKey();
         }
+
+        #region Project Processing
+        private static void GatherAndProcessProjects(string path)
+        {
+            log.Info($"Processing log file \"{Path.GetFileName(path)}\"");
+            var projectRawData = File.ReadAllLines(path);
+            foreach (string projectRaw in projectRawData)
+            {
+                Project project = new(projectRaw.Split('|'));
+                if (project.ContainsCriticalNullValues)
+                {
+                    continue;
+                }
+                bool isWantedCity = Cities.Any(c => project.City.ToUpper().Equals(c));
+                if (project != null)
+                {
+                    if (isWantedCity)
+                    {
+                        log.Debug($"Project matches criteria: {project.JobNumber}");
+                        ProcessProject(project);
+                    }
+                    else
+                    {
+                        log.Debug($"Project discarded for non-wanted city");
+                    }
+                }
+            }
+        }
+
+        private static void ProcessProject(Project project)
+        {
+            log.Info($"Processing Project {project.JobNumber} in {project.City}");
+            foreach (string file in GetAllTextFiles(project.Path))
+            {
+                HandleFile(file, project);
+            }
+        }
+
+        private static List<string> GetAllTextFiles(string projectPath)
+        {
+            log.Debug($"Project path: {projectPath}");
+            if (!Directory.Exists(projectPath))
+            {
+                log.Warn("Project does not have a file on the server.");
+                return new List<string> { };
+            }
+            List<string> textFiles = Directory.GetFiles(projectPath, "*.txt", SearchOption.AllDirectories).ToList();
+            log.Debug($"Text files found: {textFiles.Count}");
+            return textFiles;
+        }
+
+        /// <summary>
+        /// Reads a text file and grabs all control points.
+        /// </summary>
+        /// <param name="path">text file path.</param>
+        private static void HandleFile(string path, Project project)
+        {
+            log.Info($"Processing File: {path}");
+            var fileLines = File.ReadAllLines(path);
+            foreach (string line in fileLines)
+            {
+                log.Debug(line);
+                bool containsWhitelist = KeywordWhitelist.Any(s => line.Contains(s));
+                bool containsBlacklist = KeywordBlacklist.Any(s => line.Contains(s));
+                if (containsWhitelist && !containsBlacklist)
+                {
+                    string lineWFormat = line + "," + project.JobNumber;
+                    bool alreadyFound = FoundPoints.Any(p => lineWFormat.Equals(p));
+                    if (alreadyFound)
+                    {
+                        log.Debug($"Duplicate point: {line}");
+                    }
+                    else
+                    {
+                        log.Info($"Found point: {line}");
+                        FoundPoints.Add(lineWFormat);
+                    }
+                }
+                else
+                {
+                    log.Debug($"Discarded point: {line}");
+                }
+            }
+        }
+        #endregion
+
+        #region Establish Job Parameters
+        private static void EstablishJobParams()
+        {
+            log.Debug("Establishing job start and end");
+            /*Regex regex = new Regex(@"^\d{2}-\d{2}-\d{3}$");
+            log.Debug("Grabbing starting job");
+            while (string.IsNullOrEmpty(StartingJobNumber))
+            {
+                string startEntered = GetUserStringInput("Please enter the starting job number");
+                if (regex.IsMatch(startEntered))
+                {
+                    StartingJobNumber = startEntered;
+                }
+                else
+                {
+                    log.Error("Starting job number is not valid.");
+                }
+            }
+            log.Debug("Grabbing ending job");
+            regex = new Regex(@"^(?:\d{2}-\d{2}-\d{3})$|^(?:\*)$");
+            while (string.IsNullOrEmpty(EndingJobNumber))
+            {
+                string endjob = GetUserStringInput("Please enter the starting job number or * for last found job number");
+                if (regex.IsMatch(endjob))
+                {
+                    EndingJobNumber = endjob;
+                }
+                else
+                {
+                    log.Error("Ending job number is not valid.");
+                }
+            }*/
+            log.Debug("Grabbing cities");
+            {
+                bool citiesAdded = false;
+                string cities = "";
+                (int _, int top) = Console.GetCursorPosition();
+                while (!citiesAdded) 
+                {
+                    ClearConsoleAtPoint(top);
+                    Console.WriteLine($"Cites: {cities}");
+                    string city = GetUserStringInput("Please enter a city to collect");
+                    if (string.IsNullOrEmpty(city))
+                    {
+                        citiesAdded = true;
+                    }
+                    else
+                    {
+                        cities += city.ToUpper() + ", ";
+                        Cities.Add(city.ToUpper());
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region User Input
+        /// <summary>
+        /// Get string from user.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
+        /// <returns>string of input</returns>
+        private static string GetUserStringInput(string message)
+        {
+            Console.Write($"{message}: ");
+            string userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                return "";
+            }
+            else
+            {
+                return userInput;
+            }
+        }
+
+        private static void ClearConsoleAtPoint(int top)
+        {
+            int consoleHeight = Console.WindowHeight;
+            int consoleWidth = Console.WindowWidth;
+            Console.SetCursorPosition(0, top);
+            string blank = new string(' ', consoleWidth);
+            for (int i = 0; i < consoleHeight-top; i++)
+            {
+                Console.WriteLine(blank);
+            }
+            Console.SetCursorPosition(0, top);
+        }
+        #endregion
     }
 }
---
+++ ConsoleSniffer/Project.cs
@@ -0,0 +1,104 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSniffer
+{
+    public class Project
+    {
+        private static readonly ILog log = log4net.LogManager.GetLogger("Project.cs");
+
+        /// <summary>
+        /// The job number of the project.
+        /// </summary>
+        public string JobNumber { get; set; }
+
+        /// <summary>
+        /// Customer name.
+        /// </summary>
+        public string Customer { get; set; }
+
+        /// <summary>
+        /// Address or parcel information
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// City Name
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// ZIP Code
+        /// </summary>
+        public string ZIP { get; set; }
+
+        /// <summary>
+        /// Project description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Project Notes
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Project path on server.
+        /// </summary>
+        public string Path { 
+            get {
+                string[] jobNumber = JobNumber.Split('-');
+                string year = $"20{jobNumber[0]}";
+                string month = jobNumber[1];
+                string project = jobNumber[2];
+                return $@"Z:\{year}\{month}-{year}\{month}-{project}";
+            } 
+        }
+
+        public bool ContainsCriticalNullValues
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(JobNumber) ||
+                    string.IsNullOrEmpty(City))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+        public Project(string[] lineItem)
+        {
+            try
+            {
+                string jobNumber = lineItem[0];
+                string customer = lineItem[1];
+                if (string.IsNullOrEmpty(jobNumber) || string.IsNullOrEmpty(customer))
+                {
+                    log.Info($"line discarded for inadequate info: {string.Join(',', lineItem)}");
+                }
+                JobNumber = lineItem[0];
+                Customer = lineItem[1];
+                Address = lineItem[2];
+                City = lineItem[3];
+                ZIP = lineItem[4];
+                Description = lineItem[5];
+                Notes = lineItem[6];
+            }
+            catch(IndexOutOfRangeException)
+            {
+                log.Warn("Project did not have enough arguments (probably a formatting issue)");
+            }
+            catch(Exception e)
+            {
+                log.Error("Error processing project", e);
+            }
+        }
+    }
+}
